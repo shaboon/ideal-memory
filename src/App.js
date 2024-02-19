78016e4520c39b710f2c01c4653d84ae546fc309
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-02-19 12:08:36 -0500

    installed react-spring/parallax
    Added react-spring/parallax
    
    Maybe I'll add react-spring too

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
 import Footer from "./components/footer";
 
 import Portfolio from "./pages/portfolio";
@@ -12,23 +14,39 @@ function App() {
 
   return (
     <div className="App h-screen w-screen bg-black bg-gradient-to-t from-current to-gray-700 grid grid-cols-5 grid-rows-5 gap-4">
-      <header className="App-header col-span-3 row-span-3 m-8">
-        <h1 className="intro text-gray-200 text-5xl">
-          <code className="underline">
-            console.log("Hi, my name is Sean Haboon")
-          </code>
-        </h1>
-      </header>
+      <header className="App-header col-span-3 row-span-3 m-8"></header>
 
-      <section className="row-span-2 col-start-5 row-start-3 m-8">
-        <p className="text-gray-200 text-2xl">
-          I am a full stack web developer with a passion for creating
-          applications that are both functional and visually appealing. I am
-          proficient in HTML, CSS, JavaScript, and React. I am also experienced
-          in Node.js, Express, MySQL, and MongoDB. I am a quick learner and
-          eager to expand my knowledge and skills.
-        </p>
-      </section>
+      <Parallax pages={3}>
+        <ParallaxLayer offset={0}>
+          <div className="bg-black bg-gradient-to-t from-current to-gray-700">
+            <p>Scroll down</p>
+            <h1 className="intro text-gray-200 text-5xl">
+              <code className="underline">
+                console.log("Hi, my name is Sean Haboon")
+              </code>
+            </h1>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <div className="bg-black bg-gradient-to-t from-current to-gray-700">
+            <p>Scroll down</p>
+            <section className="row-span-2 col-start-5 row-start-3 m-8">
+              <p className="text-gray-200 text-2xl">
+                I am a full stack web developer with a passion for creating
+                applications that are both functional and visually appealing. I
+                am proficient in HTML, CSS, JavaScript, and React. I am also
+                experienced in Node.js, Express, MySQL, and MongoDB. I am a
+                quick learner and eager to expand my knowledge and skills.
+              </p>
+            </section>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2}>
+          <div className="bg-black bg-gradient-to-t from-current to-gray-700">
+            <p>Scroll down</p>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
 
       <Footer />
 
