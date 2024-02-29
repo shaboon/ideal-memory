bf02cd9f7aadaa1722b27bf86961b1b305abbd55
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-02-29 11:43:34 -0500

    Project Carousel Added for Modal
    Added code for project carousel

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,10 +1,30 @@
-export default function Modal() {
+import { React } from "react";
+import "./modal.css";
+
+function Modal() {
   return (
     <div className="modal">
       <div className="modal-content">
         <button className="close-button">&times;</button>
-        <p>Some text in the Modal..</p>
+        <section class="slider-wrapper">
+          <button class="slide-arrow" id="slide-arrow-prev">
+            &#8249;
+          </button>
+
+          <button class="slide-arrow" id="slide-arrow-next">
+            &#8250;
+          </button>
+
+          <ul class="slides-container" id="slides-container">
+            <li class="slide"></li>
+            <li class="slide"></li>
+            <li class="slide"></li>
+            <li class="slide"></li>
+          </ul>
+        </section>
       </div>
     </div>
   );
 }
+
+export default Modal;
