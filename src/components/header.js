4db104e340e3201dc34e9a15adf1c642993b057e
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-02-29 11:58:36 -0500

    Added Scroll Math
    Could be used to trigger Modal or scroll to the top of the page perhaps
    
    There's some functionality I could def use there to do stuffs

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,16 @@
 export default function Header() {
+  // Scroll Percentage/Progress Bar Math - Could Be Used with Modal to ask to contact me perhaps?
+  $(window).on("scroll", function () {
+    var s = $(window).scrollTop(),
+      d = $(document).height(),
+      c = $(window).height();
+
+    var scrollPercent = (s / (d - c)) * 100;
+
+    console.clear();
+    console.log(scrollPercent);
+  });
+
   return (
     <footer className="footer w-screen absolute bottom-0 left-0 bg-black bg-gradient-to-t from-current to-gray-700">
       <ul>
