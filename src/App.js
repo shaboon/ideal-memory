619917ab10bf354a8f7050aff756617fe54fae5a
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-03-06 12:31:57 -0500

    Staging for Carousel
    Prepping Repo for new Carousel component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 import Header from "./components/header";
-import Carousel from "./components/Carousel";
 import Modal, { toggleModal } from "./components/Modal.tsx";
 
 import Saturn from "./assets/photos/transparent-saturn-representation-of-orange-saturn-with-golden-ring65c9afa0bbe055.4840335117077165127696.png";
@@ -56,7 +55,6 @@ function App() {
         <ParallaxLayer offset={1.0} speed={0.5}>
           <div className="p-5 text-right text-5xl text-base-4 bg-gradient-to-l from-gray-950 to-transparent text-white">
             <p>My Projects are cool</p>
-            <Carousel />
           </div>
         </ParallaxLayer>
 
