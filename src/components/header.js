393176dc117c8e6f5e93f7e61589b04ee0d5c83a
Sean Haboon <116582145+shaboon@users.noreply.github.com>
2024-03-13 13:15:12 -0400

    prepped list items for header links/nav
    Nav for header prepped

--- src/components/header.js
+++ src/components/header.js
@@ -14,24 +14,32 @@ export default function Header() {
   return (
     <header className="header w-screen absolute bottom-0 left-0 bg-black bg-gradient-to-t from-current to-gray-700">
       <ul>
-        <li className="">
-          <a className="">
-            <img></img>
+        <li className="github">
+          <a className="" href="">
+            <button>
+              <img></img>
+            </button>
           </a>
         </li>
-        <li className="">
-          <a className="">
-            <img></img>
+        <li className="resume">
+          <a className="" href="/resume">
+            <button>
+              <img></img>
+            </button>
           </a>
         </li>
-        <li className="">
-          <a className="">
-            <img></img>
+        <li className="home">
+          <a className="" href="/">
+            <button>
+              <img></img>
+            </button>
           </a>
         </li>
-        <li className="">
+        <li className="linkedIn">
           <a className="">
-            <img></img>
+            <button>
+              <img></img>
+            </button>
           </a>
         </li>
       </ul>
