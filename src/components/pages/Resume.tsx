fd04c0ede25bd1bf019f23874db93e62faa4293b
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-04-08 11:32:17 -0400

    Resume Images restrained to max dimensions
    added max dimensions to resume images (set at 500px for both)

--- src/components/pages/Resume.tsx
+++ src/components/pages/Resume.tsx
@@ -13,7 +13,7 @@ import intStell from "../../assets/photos/project-images/interstellar-index.hero
 export default function Resume () {
     return (
         <div className="text-white">
-            <Parallax pages={1.2}>
+            <Parallax pages={2}>
                 <ParallaxLayer offset={0} speed={1.5}>
                     <section className="bg-black">
                         <h2 className="">Experiences</h2>
@@ -45,32 +45,32 @@ export default function Resume () {
                             <ul className="projectList bg-black m-2">
                                 <li>
                                     <h3 className="">SQL Based CMS Application</h3>
-                                    <img src={cmsProject} alt="CMS Project"/>
+                                    <img src={cmsProject} alt="CMS Project" className="resume-proj-img"/>
                                     <p className="">This project was created with the intent of testing SQL functionality, however, it can also be applied and utilized in a commerical setting as well. For this application, I focused on creating relational data and create calling functions that displayed the desired information.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Password Generator</h3>
-                                    <img src={passGen} alt="Regex Password Generator"/>
+                                    <img src={passGen} alt="Regex Password Generator" className="resume-proj-img"/>
                                     <p className="">This Project mainly focused on using regex to randomly generate secure passwords.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Work Day Task Tracker</h3>
-                                    <img src={workDay} alt="Work Day Task Tracker"/>
+                                    <img src={workDay} alt="Work Day Task Tracker" className="resume-proj-img"/>
                                     <p className="">By utilizing native unix time functions, I was able to create an application that tracks the user's current time and save what tasks they desire to achieve locally on their computer.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Isle Finis Notics</h3>
-                                    <img src={finisNoctis} alt="Isle Finis Noctis"/>
+                                    <img src={finisNoctis} alt="Isle Finis Noctis" className="resume-proj-img"/>
                                     <p className="">This application was made in both parody and as an exercise for teamwork. Within the project, both Bootstrap and custom CSS was applied in creating a HTML5 based application. Javscript was used to capture user-inputted data and stored locally.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Marvel Database</h3>
-                                    <img src={marvelData} alt="Marvel Database"/>
+                                    <img src={marvelData} alt="Marvel Database" className="resume-proj-img"/>
                                     <p className="">With handlebars.js and Bootstrap, this application pulls data from our own database and displays information.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Interstellar Index</h3>
-                                    <img src={intStell} alt="InterStellar Index"/>
+                                    <img src={intStell} alt="InterStellar Index" className="resume-proj-img"/>
                                     <p className="">Applying React.JS, this application uses multiple pages to display information, from our own database, about Star Wars. The intention as a team was to find a way to make an application that makes following a single character's story much easier.</p>
                                 </li>
                             </ul>
