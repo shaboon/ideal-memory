34ac32e072e1c733fe3e6b8affce316428ecb014
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-06-20 12:20:06 -0400

    Added a Modal component
    Added a modal component, this is to showcase the day-night slider

--- src/components/Style-Switch/Style-Switch.tsx
+++ src/components/Style-Switch/Style-Switch.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+import Modal from "../Modal/Modal.tsx";
+
+
 
 import "./style-switch.css";
 
 export default function StyleSwitch() {
     return (
-        <div className="style-switch flex items-center">
-        <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-        </label>
+        <div className="container flex items-center">
+            <div className="style-switch flex items-center">
+                <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+            </div>
         </div>
     );
 }
