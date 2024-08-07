f564355830b6966f68f7bfca5ef333a571fdb3cf
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-08-07 10:48:58 -0400

    Summary styling started
    Worked on styling for resume page
    
    I think I want to work in the scroll aspect of that parallax - make sue of the package I've installed

--- src/components/pages/Resume.tsx
+++ src/components/pages/Resume.tsx
@@ -12,15 +12,9 @@ import intStell from "../../assets/photos/project-images/interstellar-index.hero
 
 export default function Resume () {
     return (
-        <div className="text-white">
+        <div className="text-white flex">
             <Parallax pages={2}>
                 <ParallaxLayer offset={0} speed={1.5}>
-                    <section className="bg-black">
-                        <h2 className="">Experiences</h2>
-                        <p>My work history lies mainly in industrial manufacturing and quality assurance, however my most recent experience in the bootcamp has broadened my horizons in the tech sphere.</p>
-                    </section>
-                </ParallaxLayer>
-                <ParallaxLayer offset={0.2} speed={1.5}>
                     <section className="bg-black">
                         <h2 className="">Skills</h2>
                         <ul className="skillsList bg-black m-2">
@@ -42,38 +36,48 @@ export default function Resume () {
                 <ParallaxLayer offset={0.4} speed={1.5}>
                     <section className="bg-black">
                         <h2 className="">Projects - Expanded</h2>
-                            <ul className="projectList bg-black m-2">
+                            <ul className="projectList bg-black m-2 flex flex-wrap">
                                 <li>
                                     <h3 className="">SQL Based CMS Application</h3>
                                     <img src={cmsProject} alt="CMS Project" className="resume-proj-img"/>
-                                    <p className="">This project was created with the intent of testing SQL functionality, however, it can also be applied and utilized in a commerical setting as well. For this application, I focused on creating relational data and create calling functions that displayed the desired information.</p>
+                                    <p className="w-1/2 flex flex-wrap">This project was created with the intent of testing SQL functionality, however, it can also be applied and utilized in a commerical setting as well. For this application, I focused on creating relational data and create calling functions that displayed the desired information.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Password Generator</h3>
                                     <img src={passGen} alt="Regex Password Generator" className="resume-proj-img"/>
-                                    <p className="">This Project mainly focused on using regex to randomly generate secure passwords.</p>
+                                    <p className="w-1/2 flex flex-wrap">This Project mainly focused on using regex to randomly generate secure passwords.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Work Day Task Tracker</h3>
                                     <img src={workDay} alt="Work Day Task Tracker" className="resume-proj-img"/>
-                                    <p className="">By utilizing native unix time functions, I was able to create an application that tracks the user's current time and save what tasks they desire to achieve locally on their computer.</p>
+                                    <p className="w-1/2 flex flex-wrap">By utilizing native unix time functions, I was able to create an application that tracks the user's current time and save what tasks they desire to achieve locally on their computer.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Isle Finis Notics</h3>
                                     <img src={finisNoctis} alt="Isle Finis Noctis" className="resume-proj-img"/>
-                                    <p className="">This application was made in both parody and as an exercise for teamwork. Within the project, both Bootstrap and custom CSS was applied in creating a HTML5 based application. Javscript was used to capture user-inputted data and stored locally.</p>
+                                    <p className="w-1/2 flex flex-wrap">This application was made in both parody and as an exercise for teamwork. Within the project, both Bootstrap and custom CSS was applied in creating a HTML5 based application. Javscript was used to capture user-inputted data and stored locally.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Marvel Database</h3>
                                     <img src={marvelData} alt="Marvel Database" className="resume-proj-img"/>
-                                    <p className="">With handlebars.js and Bootstrap, this application pulls data from our own database and displays information.</p>
+                                    <p className="w-1/2 flex flex-wrap">With handlebars.js and Bootstrap, this application pulls data from our own database and displays information.</p>
                                 </li>
                                 <li>
                                     <h3 className="">Interstellar Index</h3>
                                     <img src={intStell} alt="InterStellar Index" className="resume-proj-img"/>
-                                    <p className="">Applying React.JS, this application uses multiple pages to display information, from our own database, about Star Wars. The intention as a team was to find a way to make an application that makes following a single character's story much easier.</p>
+                                    <p className="w-1/2 flex flex-wrap">Applying React.JS, this application uses multiple pages to display information, from our own database, about Star Wars. The intention as a team was to find a way to make an application that makes following a single character's story much easier.</p>
                                 </li>
                             </ul>
+                    </section>
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={0.8} speed={1.5}>
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={1} speed={1.5}>
+                    <section className="bg-black">
+                        <h2 className="">Experiences</h2>
+                        <p>My work history lies mainly in industrial manufacturing and quality assurance, however my most recent experience in the bootcamp has broadened my horizons in the tech sphere.</p>
                     </section>
                 </ParallaxLayer>
             </Parallax>
